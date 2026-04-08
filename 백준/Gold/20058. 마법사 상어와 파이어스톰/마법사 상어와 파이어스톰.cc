8094440080efae76d5b4624a154d8d802561b92b
maeng <89590797+maeng-kim@users.noreply.github.com>
2026-04-08 14:13:41 +0900

    [Gold III] Title: 마법사 상어와 파이어스톰, Time: 136 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Gold/20058. 마법사 상어와 파이어스톰/마법사 상어와 파이어스톰.cc
+++ 백준/Gold/20058. 마법사 상어와 파이어스톰/마법사 상어와 파이어스톰.cc
@@ -4,30 +4,28 @@
 using namespace std;
 
 int N, Q;
-int A[64][64];
+int chuck, sum;
+int A[65][65];
 int L[1001];
-int sum, chunk;
-bool visited[64][64];
+bool visited[65][65];
+int dx[] = {0,0,1,-1};
+int dy[] = {1,-1,0,0};
 
-const int dx[] = {0, 0, 1, -1};
-const int dy[] = {1, -1, 0, 0};
-
-void rotate(int l, int len) {
-    int size = 1 << l; 
-
-    for (int i =0; i<len; i += size) {
-        for (int j=0; j<len; j += size) {
+void rotate(int l, int n) {
+    int size = 1 << l;
+    
+    for (int i=0; i< n; i = i+size) {
+        for (int j=0; j <n; j = j+size) {
             int temp[64][64] = {};
             
-            //A(원본) => temp(회전결과 복사) => A(덮어쓰기)
-            for (int r=0; r < size; r++) {
+            for (int r=0; r <size; r++) {
                 for (int c=0; c <size; c++) {
-                    temp[c][size - r -1] = A[i+r][j+c];
+                    temp[c][size - r -1] = A[r+i][c+j]; //블록의 시작점 더하기
                 }
             }
             
-            for (int r=0; r < size; r++) {
-                for (int c=0; c<size; c++) {
+            for (int r=0; r <size; r++) {
+                for (int c =0; c <size; c++) {
                     A[i+r][j+c] = temp[r][c];
                 }
             }
@@ -35,19 +33,20 @@ void rotate(int l, int len) {
     }
 }
 
-void melt(int len) {
-    bool will_melt[64][64] = {}; //녹일 칸 표시
+void melt(int l) {
+    bool will_melt[64][64] = {};
     
-    for (int i=0; i <len; i++) {
-        for (int j=0; j<len; j++) {
+    for (int i=0; i<l; i++) {
+        for (int j=0; j <l; j++) {
             if (A[i][j] == 0) continue;
             
             int cnt =0;
-            for (int w =0; w< 4; w++) {
+            
+            for (int w =0; w <4; w++) {
                 int nx = i + dx[w];
                 int ny = j + dy[w];
                 
-                if (nx < 0 || nx >= len || ny < 0 || ny >= len) {
+                if (nx <0 || nx >= l || ny <0 || ny >= l) {
                     continue;
                 }
                 
@@ -59,9 +58,9 @@ void melt(int len) {
         }
     }
     
-    for (int i =0; i < len; i++) {
-        for (int j=0; j < len; j++) {
-            if (will_melt[i][j]) --A[i][j];
+    for (int i=0; i < l; i++) {
+        for (int j=0; j<l; j++) {
+            if (will_melt[i][j]) --A[i][j]; 
         }
     }
 }
@@ -77,57 +76,61 @@ void dfs(int sx, int sy, int s) {
         int y = st.top().second;
         st.pop();
         
-        for (int i=0; i<4; i++) {
+        for (int i=0; i <4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
             
-            if (nx <0 || nx >= s || ny <0 || ny >= s) continue;
+            if (nx <0 || nx >=s || ny <0 || ny >=s) {
+                continue;
+            }
             if (visited[nx][ny]) continue;
             if (A[nx][ny] > 0) {
-                st.push({nx, ny});
-                visited[nx][ny] = true;
                 cnt++;
+                visited[nx][ny] = true;
+                st.push({nx,ny});
             }
         }
     }
-    
-    chunk = max(chunk, cnt);
-    
+    chuck = max(chuck, cnt);
 }
 
 int main()
 {
-    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     
     cin >> N >> Q;
+    
+    sum =0;
+    chuck = 0;
     int len = 1 << N;
     
-    sum = 0;
-    
-    for (int i =0; i < len; i++) {
-        for (int j =0; j < len; j++) {
+    for (int i =0; i< len; i++) {
+        for (int j=0; j< len; j++) {
             cin >> A[i][j];
         }
     }
     
     for (int i=0; i < Q; i++) {
         cin >> L[i];
+    }
+    
+    for (int i=0; i < Q; i++) {
         rotate(L[i], len);
         melt(len);
     }
     
     for (int i=0; i<len; i++) {
         for (int j=0; j<len; j++) {
-            sum += A[i][j];
-            if (A[i][j] > 0 && !visited[i][j]) {
-                dfs(i, j, len); //덩어리 탐색
+            if (!visited[i][j] && A[i][j] > 0) {
+                dfs(i, j, len);
             }
+            sum += A[i][j];
         }
     }
     
-    cout << sum  << "\n" << chunk;
+    cout << sum << "\n" << chuck;
     
     
     return 0;
